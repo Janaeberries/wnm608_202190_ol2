f5d74b2dd25b0bf631ae3d1ea62df4222ea29636
Janae Smith <janaesmith@janaes-imac.attlocal.net>
2021-10-23 22:49:00 -0500

    Mod7

---
+++ smith.janae/Mod7/parts/meta.php
@@ -0,0 +1,6 @@
+
+	<meta charset="UTF-8">
+
+	<link rel="stylesheet" href="Mod7/lib/css/styleguide.css">
+    <link rel="stylesheet" href="Mod7/css/storetheme.css">
+    <link rel="stylesheet" href="Mod7/lib/css/gridsystem.css">
---
+++ smith.janae/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Anaberry Skincare</title>
+
+	<link rel="stylesheet" href="Mod7/lib/css/styleguide.css">
+    <link rel="stylesheet" href="Mod7/css/storetheme.css">
+    <link rel="stylesheet" href="Mod7/lib/css/gridsystem.css">
+
+</head>
+<body>
+
+<header class="navbar">
+		<div class="container display-flex">
+			<div class="flex-none">
+				<h1>Anaberry Skincare</h1>
+			</div>
+			<div class="flex-stretch"></div>
+			<nav class="nav nav-flex flex-none">
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="product_list.php">Store</a></li>
+					<li><a href="#">Cart</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+
+	<div class="view-window" style="background-image: url(Mod7/img/skin_models.jpg);"></div>
+
+	<div class="container">
+		<div class="card hard">
+			<h2>Product List</h2>
+
+			<div class="grid gap xs-small md-medium">
+				<div class="col-xs-6 col-md-3">
+					<div class="card soft">
+					<figure class="figure">
+						<img src="Mod7/img/cleanser_bottle.jpeg" alt>
+						<div>Cleansers</div>
+					</figure>
+				</div>
+				</div>
+				<div class="col-xs-6 col-md-3">
+					<div class="card soft">
+					<figure class="figure">
+						<img src="Mod7/img/serum_bottle.jpg" alt>
+						<div>Serums</div>
+					</figure>
+				</div>
+				</div>
+				<div class="col-xs-6 col-md-3">
+					<div class="card soft">
+					<figure class="figure">
+						<img src="Mod7/img/toner.jpeg" alt>
+						<div>Toners</div>
+					</figure>
+				</div>
+				</div>
+				<div class="col-xs-6 col-md-3">
+					<div class="card soft">
+					<figure class="figure">
+						<img src="Mod7/img/jar.jpeg" alt>
+						<div>Moisturizers</div>
+					</figure>
+				</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+
+				<div class="container">
+					<div class="card hard">
+					<h2>Our Mission</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras in tellus purus. Nullam pharetra ante at lorem tincidunt, nec iaculis nisi ullamcorper. Quisque vehicula at eros sit amet mattis. Donec malesuada dui vitae purus tincidunt placerat. Fusce aliquet nunc rutrum, cursus lectus et, gravida nisl. Curabitur scelerisque orci eu neque pharetra vulputate. Aliquam tortor lacus, accumsan ullamcorper auctor eget, tincidunt eu tellus. Mauris lobortis odio vel dolor imperdiet, ac eleifend libero mollis. Vivamus venenatis tortor sed sapien rhoncus, nec egestas nulla facilisis. Phasellus magna lorem, porttitor id consectetur gravida, gravida imperdiet libero. Morbi sed nulla sem. Nullam faucibus venenatis suscipit. Fusce volutpat pulvinar vehicula. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+				</div>
+				</div>
+				</div>
+
+
+		</div>
+	</div>
+	
+</body>
+</html>
