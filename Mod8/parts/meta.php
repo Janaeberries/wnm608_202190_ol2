7b436c55341956ce05b0176ead603a591a68257d
Janae Smith <janaesmith@janaes-imac.attlocal.net>
2021-10-31 09:25:21 -0500

    Mod8

---
+++ Mod8/parts/meta.php
@@ -0,0 +1,7 @@
+<meta name="viewport" content="width=device-width">
+
+<base href="https://janaesmith.com/aau/wnm608">
+
+<link rel="stylesheet" href="lib/css/storetheme.css">
+<link rel="stylesheet" href="lib/css/gridsystem.css">
+<link rel="stylesheet" href="../css/reset.css">
