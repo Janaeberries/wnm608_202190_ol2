55e6b5da2becd222d555fab25cd95d39f7849f35
Janae Smith <janaesmith@janaes-imac.attlocal.net>
2021-12-03 14:31:43 -0600

    Search and Admin updates

--- smith.janae/product_list.php
+++ smith.janae/product_list.php
@@ -35,9 +35,9 @@
 
  ?>
 
-    <script src="../js/product_list.js"></script>
-    <script src="../lib/js/functions.js"></script>
-    <script src="../js/templates.js"></script>
+    <script src="../smith.janae/resources/js/product_list.js"></script>
+    <script src="../smith.janae/resources/lib/js/functions.js"></script>
+    <script src="../smith.janae/resources/js/templates.js"></script>
 
 
      <div class="container">
@@ -46,7 +46,44 @@
                 <input type="search" placeholder="Products">
             </form>
         </div>
+
+        <div class="form-control">
+            <div class="card soft">
+            <div class="display flex">
+                <div class="flex-stretch display-flex">
+            <div class="flex-none">
+                <button data-filter type="button" class="form-button">All</button>
+            </div>
+            <div class="flex-none">
+                <button data-filter="category" data-value="cleansers" type="button" class="form-button">cleansers</button>
+            </div>
+            <div class="flex-none">
+                <button data-filter="category" data-value="toners" type="button" class="form-button">toners</button>
+            </div>
+            <div class="flex-none">
+                <button data-filter="category" data-value="serums" type="button" class="form-button">serums</button>
+            </div>
+            <div class="flex-none">
+                <button data-filter="category" data-value="moisturizers" type="button" class="form-button">moisturizers</button>
+            </div>
+        </div>
     </div>
+</div>
+
+        <div class="flex-none">
+            <div class="form-select">
+            <select class="js-sort">
+                <option value="1">Lowest to Highest</option>
+                <option value="2">Highest to Lowest</option>
+                <option value="3">Newest</option>
+                <option value="4">Oldest</option>
+            </select>
+        </div>
+    </div>
+</div>
+
+
+
 
 	<div class="view-window" style="background-image: url(resources/img/skincare_woman.png);"></div>
 
