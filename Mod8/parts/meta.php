11310cc14ec36b1daa0e728fdeac82c1c5a747de
Janaeberries <86499853+Janaeberries@users.noreply.github.com>
2021-12-17 18:44:09 -0600

    Delete Mod8 directory

--- Mod8/parts/meta.php
+++
@@ -1,7 +0,0 @@
-<meta name="viewport" content="width=device-width">
-
-<base href="https://janaesmith.com/aau/wnm608">
-
-<link rel="stylesheet" href="lib/css/storetheme.css">
-<link rel="stylesheet" href="lib/css/gridsystem.css">
-<link rel="stylesheet" href="../css/reset.css">
